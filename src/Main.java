83866901b0af2b69bdaf1cf79ede7a7074685503
Vasco Conde <vc@vascoconde.com>
2013-05-21 18:34:57 +0200

    Important Changes.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Delft");
+		System.out.println("Hello San Francisco");
+
 	}
 
 }
